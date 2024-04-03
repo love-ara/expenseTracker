cd52ea491d224a93af09d3cd60f3a12e0b52bb97
love-ara <aramideotenaike@gmail.com>
2024-04-03 21:40:59 +0100

    commit

--- src/main/java/africa/semicolon/expenseTracker/data/model/User.java
+++ src/main/java/africa/semicolon/expenseTracker/data/model/User.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,5 +17,5 @@ public class User {
     private String name;
     private String email;
     @DBRef
-    private ExpenseCategory expenseCategory;
+    private List<ExpenseCategory> expenseCategory;
 }
