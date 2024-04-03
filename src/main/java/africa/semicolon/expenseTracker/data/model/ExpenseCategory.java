27df30ff1ddf139a61f8e547224470694530017b
love-ara <aramideotenaike@gmail.com>
2024-04-03 21:02:34 +0100

    commit

---
+++ src/main/java/africa/semicolon/expenseTracker/data/model/ExpenseCategory.java
@@ -0,0 +1,14 @@
+package africa.semicolon.expenseTracker.data.model;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document("Category")
+public class ExpenseCategory {
+    @Id
+    private String id;
+    private String categoryName;
+
+}
