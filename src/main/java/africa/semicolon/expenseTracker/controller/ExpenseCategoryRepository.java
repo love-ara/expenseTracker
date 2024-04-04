d35a72109551b87984fbaef8dced27efe5799a01
love-ara <aramideotenaike@gmail.com>
2024-04-04 04:24:17 +0100

    commit

--- src/main/java/africa/semicolon/expenseTracker/controller/ExpenseCategoryRepository.java
+++ src/main/java/africa/semicolon/expenseTracker/controller/ExpenseCategoryRepository.java
@@ -2,6 +2,10 @@ package africa.semicolon.expenseTracker.controller;
 
 import africa.semicolon.expenseTracker.data.model.ExpenseCategory;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ExpenseCategoryRepository extends MongoRepository<ExpenseCategory, String > {
+@Repository
+public interface ExpenseCategoryRepository extends MongoRepository<ExpenseCategory, String>{
+    ExpenseCategory findExpenseCategoriesByCategoryName(String categoryName);
+
 }
