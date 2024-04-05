4b45c47efcc11145d1a56c83a50a4bd16e33f979
love-ara <aramideotenaike@gmail.com>
2024-04-05 06:01:54 +0100

    commit

--- src/main/java/africa/semicolon/expenseTracker/controller/ExpenseCategoryController.java
+++ src/main/java/africa/semicolon/expenseTracker/controller/ExpenseCategoryController.java
@@ -5,11 +5,11 @@ import africa.semicolon.expenseTracker.data.repository.ExpenseCategoryRepository
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +28,20 @@ public class ExpenseCategoryController {
     Optional<ExpenseCategory> expenseCategory = expenseCategoryRepository.findById(id);
     return expenseCategory.map(response -> ResponseEntity.ok().body(response)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
+    @PostMapping("/category")
+    public ResponseEntity<ExpenseCategory> createExpenses(@Validated @RequestBody ExpenseCategory expenseCategory) throws URISyntaxException {
+            ExpenseCategory result = expenseCategoryRepository.save(expenseCategory);
+            return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
+    }
+    @PutMapping("/category/{id}")
+    public ResponseEntity<ExpenseCategory> updateExpenseCategory(@Validated @RequestBody ExpenseCategory expenseCategory){
+        ExpenseCategory result = expenseCategoryRepository.save(expenseCategory);
+        return ResponseEntity.ok().body(result);
+    }
+    @DeleteMapping("/category/{id}")
+    public ResponseEntity<?> deleteExpenseCategory(@PathVariable String id){
+        expenseCategoryRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
