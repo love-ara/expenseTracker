91613455d95a06b267c05e861dc2b8ba7785556f
love-ara <aramideotenaike@gmail.com>
2024-04-05 06:01:29 +0100

    commit

---
+++ src/main/java/africa/semicolon/expenseTracker/data/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package africa.semicolon.expenseTracker.data.repository;
+
+import africa.semicolon.expenseTracker.data.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
