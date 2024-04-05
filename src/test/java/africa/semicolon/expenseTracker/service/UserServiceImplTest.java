448bceb6f422220dc74340ea5172264b4337ca37
love-ara <aramideotenaike@gmail.com>
2024-04-05 06:33:37 +0100

    commit

---
+++ src/test/java/africa/semicolon/expenseTracker/service/UserServiceImplTest.java
@@ -0,0 +1,33 @@
+package africa.semicolon.expenseTracker.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+public class UserServiceImplTest {
+
+    @Test
+    public void registerUser() {
+    }
+
+    @Test
+    public void addToExpenseList() {
+    }
+
+    @Test
+    public void deleteFromExpenseList() {
+    }
+
+    @Test
+    public void updateExpenseList() {
+    }
+
+    @Test
+    public void getExpense() {
+    }
+
+    @Test
+    public void getExpenses() {
+    }
+}
