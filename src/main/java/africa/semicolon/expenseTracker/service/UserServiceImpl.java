bada045e55eb58e92f4e7d784ca212f01e382ec7
love-ara <aramideotenaike@gmail.com>
2024-04-05 06:33:16 +0100

    commit

--- src/main/java/africa/semicolon/expenseTracker/service/UserServiceImpl.java
+++ src/main/java/africa/semicolon/expenseTracker/service/UserServiceImpl.java
@@ -1,4 +1,46 @@
 package africa.semicolon.expenseTracker.service;
 
+import africa.semicolon.expenseTracker.data.model.Expense;
+import africa.semicolon.expenseTracker.data.model.User;
+import africa.semicolon.expenseTracker.dto.request.DeleteRequest;
+import africa.semicolon.expenseTracker.dto.request.ExpenseRequest;
+import africa.semicolon.expenseTracker.dto.request.RegisterRequest;
+import africa.semicolon.expenseTracker.dto.request.UpdateRequest;
+import africa.semicolon.expenseTracker.dto.response.DeleteResponse;
+import africa.semicolon.expenseTracker.dto.response.UpdateResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserServiceImpl implements UserService {
+    @Override
+    public User registerUser(RegisterRequest registerRequest) {
+        return null;
+    }
+
+    @Override
+    public Expense addToExpenseList(ExpenseRequest expenseRequest) {
+        return null;
+    }
+
+    @Override
+    public DeleteResponse deleteFromExpenseList(DeleteRequest deleteRequest) {
+        return null;
+    }
+
+    @Override
+    public UpdateResponse updateExpenseList(UpdateRequest updateRequest) {
+        return null;
+    }
+
+    @Override
+    public Expense getExpense(String id) {
+        return null;
+    }
+
+    @Override
+    public List<Expense> getExpenses() {
+        return null;
+    }
 }
