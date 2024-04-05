ac33719855887646079ecc1158e1d6d4b4f33926
love-ara <aramideotenaike@gmail.com>
2024-04-05 06:30:48 +0100

    commit

---
+++ src/test/java/africa/semicolon/expenseTracker/service/ExpenseCategoryServiceImpTest.java
@@ -0,0 +1,10 @@
+package africa.semicolon.expenseTracker.service;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class ExpenseCategoryServiceImpTest {
+
+}
